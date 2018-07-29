330f88b8a5e4965cade752410edb7a96cff28121
DevRenji <40606735+DevRenji@users.noreply.github.com>
2018-07-28 20:54:10 -0400

    Update package.py

--- package.py
+++ package.py
@@ -1,1 +1,13 @@
-
+{
+  "name": "Cleo.py",
+  "description": "A Discord bot coded in Discord.Py, hosted using Heroku.",
+  "version": "1.0.0",
+  "main": "Cleo.py",
+  "scripts": {
+    "start": "node cleo.py"
+  },
+  "dependencies": {
+    "discord.py": "11.1.0",
+    "request": "2.81.0"
+  }
+}
